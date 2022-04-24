754e31c7e0240981d35f36e3178f18fea588ee86
disha <mumudb@gmail.com>
2022-04-24 13:40:12 -0400

    add inquirer package

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,5 @@
 // TODO: Include packages needed for this application
+var inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -7,7 +8,23 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+  inquirer
+  .prompt(questions)
+  .then((answers) => {
+    console.log(answers)
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
+}
 
 // Function call to initialize app
 init();
